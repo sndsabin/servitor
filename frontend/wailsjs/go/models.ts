a24afd3f89a8ab4a0c0f8e40ebb70467ca6505bf
sndsabin <sndsabin@gmail.com>
2026-09-05 19:16:53 +0300

    sync wails bindings

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -233,3 +233,22 @@ export namespace network {
 
 }
 
+export namespace options {
+	
+	export class SecondInstanceData {
+	    Args: string[];
+	    WorkingDirectory: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new SecondInstanceData(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.Args = source["Args"];
+	        this.WorkingDirectory = source["WorkingDirectory"];
+	    }
+	}
+
+}
+
